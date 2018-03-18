3728c4ef86428b81c145284b26e0b242f3117eec
Bram <bramrenting@gmail.com>
2018-03-18 15:22:15 +0100

    added comments to wrapper.py

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # NegotiationAgent
+Make sure to install smac using:
+pip install smac
+
+
+To run the script use the "run.sh" file
--- wrapper.py
+++ wrapper.py
@@ -8,6 +8,7 @@ from subprocess import call, PIPE
 def main():
 	start = time.time()
 
+	# parse input arguments
 	parser = argparse.ArgumentParser(description='Wrapper to run Genius from SMAC')
 	parser.add_argument('instance', type=int,  help='instance')
 	parser.add_argument('specifics', type=int,  help='specifics')
@@ -18,16 +19,22 @@ def main():
 	
 	args = parser.parse_args()
 
+	# write the xml tournament setup
 	write_xml(args.params[1::2])
 
+	# call genius with tournament setup; surpress output
 	devnull = open(os.devnull, 'w')
 	call(["java", "-cp", "genius-8.0.0.jar", "genius.cli.Runner", "../runfiles/tournament.xml", "../logs/output"], cwd = "genius-8.0.0",
 			stdout=devnull, stderr=devnull) 
 
+	# process genius output file
 	status, quality = getdatafromcsv("logs/output.csv")
+
+	# calculate total runtime
 	end = time.time()
 	runtime = end - start
 
+	# print output for SMAC algorithm
 	print("Result for SMAC: {}, {}, {}, {}, {}".format(status, runtime, args.runlength, quality, args.seed))
 
 
@@ -45,6 +52,7 @@ def getdatafromcsv(file):
 	index_utils = []
 	result = []
 
+	# init variables
 	social_welfare = 0.0
 	utility = 0.0
 	count = 0
@@ -53,6 +61,7 @@ def getdatafromcsv(file):
 		if reader.line_num > 1:
 			if reader.line_num == 2:
 				for item in row:
+					# save column indices of useful data
 					if item == "Social Welfare":
 						index_sw = row.index(item)
 					if item[:5] == "Agent":
@@ -61,6 +70,7 @@ def getdatafromcsv(file):
 						index_utils.append(row.index(item))
 			else:
 				for item in row:
+					# find utility that corresponds to produced agent
 					if item[:3] == "boa":
 						social_welfare += float(row[index_sw])
 						utility += float(row[row.index(item)-min(index_agents)+min(index_utils)])
@@ -74,6 +84,7 @@ def getdatafromcsv(file):
 	csvFile.close()
 	#csvFile2.close()
 
+	# return result
 	if count == 0:
 		return ("CRASHED", 6.0)
 	else:
@@ -82,22 +93,24 @@ def getdatafromcsv(file):
 
 
 def write_xml(BOAcomps):
-
+	# open tournament template and read
 	f = open("runfiles/tournament_template.xml", "r")
 	contents = f.readlines()
 	f.close()
 
+	# add customized agent components
 	contents[16] = '<item classpath="negotiator.boaframework.offeringstrategy.{}"/>\n'.format(BOAcomps[0])
 	contents[19] = '<item classpath="negotiator.boaframework.acceptanceconditions.{}"/>\n'.format(BOAcomps[1])
 	contents[22] = '<item classpath="negotiator.boaframework.opponentmodel.{}"/>\n'.format(BOAcomps[2])
 	contents[25] = '<item classpath="negotiator.boaframework.omstrategy.{}"/>\n'.format(BOAcomps[3])
 
+	# delete old tournament file and write new
 	f = open("runfiles/tournament.xml", "w")
 	contents = "".join(contents)
 	f.write(contents)
 	f.close()
 
-
+# Check if wrapper is called via command line
 if __name__ == "__main__":
 	main()
 
